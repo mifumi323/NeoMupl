e93d79a7575e07cf117e17c686b6c6f41f1685ed
mifumi323 <mifumi323@gmail.com>
2017-10-28 22:16:54 +0900

    ちょいとファイル分離

---
+++ NeoMupl/Player/Enum.cs
@@ -0,0 +1,5 @@
+﻿namespace NeoMupl.Player
+{
+    /// <summary>演奏方法</summary>
+    public enum PlayMethod { DirectShow, DirectMusic, MCI, NAudio }
+}
---
+++ NeoMupl/Player/IMusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace NeoMupl.Player
+{
+    /// <summary>音楽再生統括クラス</summary>
+    public interface IMusicPlayer
+    {
+        MusicData Data
+        {
+            get;
+            set;
+        }
+        bool Loop
+        {
+            get;
+        }
+        bool IsPlaying
+        {
+            get;
+        }
+        double Length
+        {
+            get;
+        }
+        double Position
+        {
+            get;
+        }
+        void Play(bool bLoop);
+        void Stop();
+        void LoopMethod();
+        void Dispose();
+        void SetTempo(double tempo);
+        double Volume
+        {
+            get;
+        }
+        IEnumerable<string> GetDirectMusicPorts();
+        void SetDirectMusicPort(string p);
+    }
+}
