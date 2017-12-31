3d81026efb42f7c20326b43294bd7aeb8ae8bf95
mifumi323 <mifumi323@gmail.com>
2017-12-31 15:00:46 +0900

    FormItemにアイコン付けた

--- NeoMupl/FormItem.Designer.cs
+++ NeoMupl/FormItem.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormItem));
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.txtNavigation = new System.Windows.Forms.TextBox();
@@ -451,6 +452,7 @@
             this.Controls.Add(this.txtNavigation);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MinimizeBox = false;
             this.Name = "FormItem";
             this.ShowInTaskbar = false;
