ad5af2a056b1b7adec4adcd839441bf60c60dfa3
mifumi323 <mifumi323@gmail.com>
2020-08-09 22:53:56 +0900

    0.32.0

--- NeoMupl/Properties/AssemblyInfo.cs
+++ NeoMupl/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.31.3.0")]
+[assembly: AssemblyVersion("0.32.0.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
