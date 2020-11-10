f6cb70fddc853d357ae9ce6665fffc3bb87509fa
mifumi323 <mifumi323@gmail.com>
2020-11-10 21:56:56 +0900

    拡張子ごとの設定のクラス
    #6

---
+++ NeoMupl/ExtensionRule.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+using NeoMupl.Player;
+
+namespace NeoMupl
+{
+    public class ExtensionRule
+    {
+        public ExtensionRule(string extension, PlayMethod playMethod)
+        {
+            Extension = extension;
+            PlayMethod = playMethod;
+        }
+
+        public string Extension { get; set; }
+        public PlayMethod PlayMethod { get; set; }
+    }
+}
