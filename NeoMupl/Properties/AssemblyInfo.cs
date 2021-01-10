d24b15efe607cfd16f53cb3ba08de2f46ee476dd
mifumi323 <mifumi323@gmail.com>
2021-01-10 14:41:49 +0900

    0.33.0

--- NeoMupl/Properties/AssemblyInfo.cs
+++ NeoMupl/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NeoMupl")]
-[assembly: AssemblyCopyright("Copyright Mifumi (C)  2007-2020")]
+[assembly: AssemblyCopyright("Copyright Mifumi (C)  2007-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.32.0.0")]
+[assembly: AssemblyVersion("0.33.0.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
