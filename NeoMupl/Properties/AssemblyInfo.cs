e06949814f6c7ce659273fd79c0440c2ee57ffe8
mifumi323 <mifumi323@gmail.com>
2021-10-07 20:32:47 +0900

    [変更]NeoMupl本体のプロパティ等を修正
    #25

--- NeoMupl/Properties/AssemblyInfo.cs
+++ NeoMupl/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NeoMupl")]
-[assembly: AssemblyCopyright("Copyright Mifumi (C)  2007-2021")]
+[assembly: AssemblyCopyright("Copyright © Mifumi 2007-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
