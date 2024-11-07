57ef9afe65e4e7924119597be72ab050f06d32b8
mifumi323 <mifumi323@gmail.com>
2024-11-07 23:46:46 +0900

    変更履歴

---
+++ Core/History/History.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+using System.Collections.Generic;
+
+namespace NeoMupl.History
+{
+    public class History
+    {
+        private readonly MusicList musicList;
+        private readonly List<IEvent> events;
+        private int index;
+
+        public History(MusicList musicList)
+        {
+            this.musicList = musicList;
+            events = new List<IEvent>();
+            index = -1;
+        }
+
+        public void Add(IEvent e)
+        {
+            if (index < events.Count - 1)
+            {
+                events.RemoveRange(index + 1, events.Count - index - 1);
+            }
+            events.Add(e);
+            index++;
+        }
+
+        public void Undo()
+        {
+            if (index >= 0)
+            {
+                events[index].Undo(musicList);
+                index--;
+            }
+        }
+
+        public void Redo()
+        {
+            if (index < events.Count - 1)
+            {
+                index++;
+                events[index].Redo(musicList);
+            }
+        }
+    }
+}
---
+++ Core/History/IEvent.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+namespace NeoMupl.History
+{
+    public interface IEvent
+    {
+        string Name { get; }
+        public void Undo(MusicList musicList);
+        public void Redo(MusicList musicList);
+    }
+}
